61dbc3aee966ebef9cd5e38b254ed83737164322
davidmwita <dmwita@student.ubc.ca>
2022-04-29 11:12:07 -0700

    modified script

--- layers_install.sh
+++ layers_install.sh
@@ -1,10 +1,13 @@
 #!/bin/zsh
+echo 'INSTALLING DEPENDENCIES...'
 echo 'Navigating to unirest layer...'
 cd ussd-app/layers/unirest/nodejs
-echo 'Installing dependencies...'
+echo 'Installing unirest...'
 npm i
+echo 'Unirest installed.'
 echo 'Navigating to ussd-menu-builder layer...'
 cd ../../ussd-menu-builder/nodejs
 echo 'Installing dependencies'
 npm i
+echo 'Ussd-menu-builder installed.'
 echo 'Done.'
